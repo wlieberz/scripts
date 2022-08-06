77a17bf9c63620f777a90f3ef776a56517bf7c8a
wlieberz <williamlieberz@gmail.com>
2022-08-06 15:59:29 -0700

    Add blank-screen.sh

---
+++ blank-screen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Because I can never remember this command.
+#
+# Works on most Linux distributions.
+# Note: does NOT lock the screen, just turns off the display.
+
+sleep 1; xset dpms s activate
